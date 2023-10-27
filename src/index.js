9c88f6de8ab3a02844ed64eefa10bad754c30502
Lucaciu Daiana-Larisa <78080241+daia-code@users.noreply.github.com>
2023-10-27 16:05:52 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 
 ReactDOM.render(
-  <Router basename="/Coffee">
+
     <App />
-  </Router>
+
   document.getElementById('root')
 );
 
